70bee3fded49f54d36dc47de79bf851856038b3e
Łukasz Piątkowski <piontec@gmail.com>
2023-09-19 16:24:40 +0200

    d20-2

--- d20_2.py
+++ d20_2.py
@@ -1,7 +1,11 @@
 from typing import Optional
 
 
+# key = 1
 key = 811589153
+# rounds = 1
+rounds = 10
+jumps_mod = 0
 
 
 class Node:
@@ -11,12 +15,12 @@ class Node:
         self.val = value
 
     def __repr__(self) -> str:
-        return f"{self.val}"
+        return f"{self.val * key}"
 
     def move(self) -> None:
-        move = self.val
-        if move == 0:
+        if self.val % jumps_mod == 0:
             return
+        move = (self.val * key) % jumps_mod if self.val > 0 else -((-self.val * key) % jumps_mod)
         # remove itself from the list
         self.prev.next = self.next
         self.next.prev = self.prev
@@ -43,21 +47,25 @@ def run(lines: list[str]) -> int:
     nums: list[int] = [int(line) for line in lines]
     msg: dict[int, Node] = {i: Node(nums[i]) for i in range(len(nums))}
     msg_len = len(msg)
+    global jumps_mod
+    jumps_mod = msg_len - 1
     zero_index = -1
     for k, v in msg.items():
         v.next = msg[(k + 1) % msg_len]
         msg[(k + 1) % msg_len].prev = v
         if v.val == 0:
             zero_index = k
-    for orig_index in msg.keys():
-        msg[orig_index].move()
+    for _ in range(rounds):
+        for orig_index in msg.keys():
+            msg[orig_index].move()
+
     res = 0
     for offset in [1000, 2000, 3000]:
         off = offset % msg_len
         cur = msg[zero_index]
         for _ in range(off):
             cur = cur.next
-        res += cur.val
+        res += cur.val * key
     return res
 
 
@@ -76,7 +84,8 @@ def test() -> None:
 -2
 0
 4""".splitlines())
-    assert res == 3
+    # assert res == 3
+    assert res == 1623178306
 
 
 if __name__ == "__main__":
