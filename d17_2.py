36cbb581f8f54b9dc1258e0d7f696077e57f6a3b
Łukasz Piątkowski <piontec@gmail.com>
2023-09-22 19:03:54 +0200

    d17-2 - first try

--- d17_2.py
+++ d17_2.py
@@ -23,15 +23,21 @@ shapes = {
 }
 
 
-def run(moves: str, rocks_limit: int) -> int:
+def run(moves: str, orig_rocks_limit: int) -> int:
     cave = np.array([[Types.WALL] * 9], dtype=np.int8)
     moves_counter = 0
-    for rock_index in range(rocks_limit):
+    rocks_counter = 0
+    cycle_height_start = 0
+    cycle_rock_index_start, cycle_rock_index_end = 0, 0
+    rocks_limit = orig_rocks_limit
+    force_simulate = False
+    remaining_rocks_count_cycles, cycle_len = 0, 0
+    while rocks_counter < rocks_limit:
         last_row_with_rock_ind = get_first_row_with_rock(cave)
         add_rows = 7 - (cave.shape[0] - last_row_with_rock_ind - 1)
         vspace = np.array(([Types.WALL] + [Types.EMPTY] * 7 + [Types.WALL]) * add_rows).reshape(add_rows, 9)
         cave = np.vstack((cave, vspace))
-        shape: np.ndarray = shapes[rock_index % 5].copy()
+        shape: np.ndarray = shapes[rocks_counter % 5].copy()
         shape_pos = (last_row_with_rock_ind + 4, 3)
         while True:
             # apply jet
@@ -49,7 +55,33 @@ def run(moves: str, rocks_limit: int) -> int:
                 cave[shape_pos[0]:shape_pos[0] + shape.shape[0], shape_pos[1]:shape_pos[1] + shape.shape[1]] += shape
                 break
             shape_pos = new_shape_pos
-    return get_first_row_with_rock(cave)
+        top_rock_row_index = get_first_row_with_rock(cave)
+        rocks_counter += 1
+
+        # let's run it for all the possible combinations of rock and jet direction
+        if rocks_counter < len(moves) * 5 or force_simulate:
+            continue
+
+        # we haven't found cycle yet
+        if cycle_len == 0:
+            # we're looking for same 3 rows: bottom one, top one, and half-way through: this means we potentialy have
+            # 2 full cycles and 1st element of the 3rd one
+            for start_index in range(top_rock_row_index // 2):
+                mid_index = start_index + (top_rock_row_index - start_index) // 2
+                if np.array_equal(cave[start_index], cave[top_rock_row_index]) and np.array_equal(cave[start_index], cave[mid_index]):
+                    cycle_len = mid_index - start_index
+                    cycle_height_start = top_rock_row_index
+                    cycle_rock_index_start = rocks_counter
+        elif cycle_rock_index_end == 0 and top_rock_row_index == cycle_height_start + cycle_len:
+            cycle_rocks_delta = rocks_counter - cycle_rock_index_start
+            remaining_rocks_count_cycles = (orig_rocks_limit - rocks_counter) // cycle_rocks_delta
+            rocks_left = (orig_rocks_limit - rocks_counter) % cycle_rocks_delta
+            assert orig_rocks_limit == rocks_counter + remaining_rocks_count_cycles * cycle_rocks_delta + rocks_left
+            force_simulate = True
+            rocks_counter += remaining_rocks_count_cycles * cycle_rocks_delta
+    res = get_first_row_with_rock(cave)
+    res += remaining_rocks_count_cycles * cycle_len
+    return res
 
 
 def get_first_row_with_rock(cave: np.ndarray) -> int:
@@ -82,13 +114,13 @@ def main() -> None:
     with open("i17.txt", "r") as i:
         lines = i.readlines()
     lines = [l.rstrip('\n') for l in lines]
-    res = run(lines[0], rocks_limit=2022)
+    res = run(lines[0], orig_rocks_limit=1000000000000)
     print(res)
 
 
 def test() -> None:
-    res = run(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>", rocks_limit=2022)
-    assert res == 3068
+    res = run(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>", orig_rocks_limit=1000000000000)
+    assert res == 1514285714288
 
 
 if __name__ == "__main__":
