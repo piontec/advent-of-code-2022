370f5fd32db44b6035cbb569088d00f409bd59a9
Łukasz Piątkowski <piontec@gmail.com>
2023-09-24 13:22:48 +0200

    d17-2 - works!

--- d17_2.py
+++ d17_2.py
@@ -1,3 +1,4 @@
+import typing
 from enum import IntEnum
 
 import numpy as np
@@ -9,6 +10,10 @@ class Types(IntEnum):
     STABLE = 2
     FALLING = 3
 
+
+RocksRow = tuple[int, int, int, int, int, int, int]
+StateKey = tuple[int, int, RocksRow]
+StateVal = tuple[int, int]
 
 shapes = {
     0: np.array([[Types.FALLING] * 4]),  # -
@@ -27,27 +32,28 @@ def run(moves: str, orig_rocks_limit: int) -> int:
     cave = np.array([[Types.WALL] * 9], dtype=np.int8)
     moves_counter = 0
     rocks_counter = 0
-    cycle_height_start = 0
-    cycle_rock_index_start, cycle_rock_index_end = 0, 0
     rocks_limit = orig_rocks_limit
     force_simulate = False
-    remaining_rocks_count_cycles, cycle_len = 0, 0
+    remaining_rock_cycles_count, cycle_height_delta = 0, 0
+    states: dict[StateKey, StateVal] = {}
     while rocks_counter < rocks_limit:
         last_row_with_rock_ind = get_first_row_with_rock(cave)
         add_rows = 7 - (cave.shape[0] - last_row_with_rock_ind - 1)
         vspace = np.array(([Types.WALL] + [Types.EMPTY] * 7 + [Types.WALL]) * add_rows).reshape(add_rows, 9)
         cave = np.vstack((cave, vspace))
-        shape: np.ndarray = shapes[rocks_counter % 5].copy()
+        rock_index = rocks_counter % 5
+        shape: np.ndarray = shapes[rock_index].copy()
         shape_pos = (last_row_with_rock_ind + 4, 3)
+        jet_move_index = 0
         while True:
             # apply jet
-            jet_move = moves[moves_counter % len(moves)]
+            jet_move_index = moves_counter % len(moves)
+            jet_move = moves[jet_move_index]
             moves_counter += 1
             shape_pos = apply_jet(jet_move, shape_pos, shape, cave)
             # move down
             new_shape_pos = (shape_pos[0] - 1, shape_pos[1])
-            cave_after_move = cave[new_shape_pos[0]:new_shape_pos[0] + shape.shape[0],
-                                   new_shape_pos[1]:new_shape_pos[1] + shape.shape[1]] + shape
+            cave_after_move = cave[new_shape_pos[0]:new_shape_pos[0] + shape.shape[0], new_shape_pos[1]:new_shape_pos[1] + shape.shape[1]] + shape
 
             has_collision = np.any(cave_after_move[:][:] > Types.FALLING)
             if has_collision:
@@ -55,32 +61,31 @@ def run(moves: str, orig_rocks_limit: int) -> int:
                 cave[shape_pos[0]:shape_pos[0] + shape.shape[0], shape_pos[1]:shape_pos[1] + shape.shape[1]] += shape
                 break
             shape_pos = new_shape_pos
-        top_rock_row_index = get_first_row_with_rock(cave)
         rocks_counter += 1
 
-        # let's run it for all the possible combinations of rock and jet direction
-        if rocks_counter < len(moves) * 5 or force_simulate:
+        if force_simulate:
             continue
 
+        top_rock_row_index = get_first_row_with_rock(cave)
+        top_shape = tuple(get_top_shape(cave))
+        state_key = (rock_index, jet_move_index, top_shape)
+        state_val = (rocks_counter, top_rock_row_index)
+
         # we haven't found cycle yet
-        if cycle_len == 0:
-            # we're looking for same 3 rows: bottom one, top one, and half-way through: this means we potentialy have
-            # 2 full cycles and 1st element of the 3rd one
-            for start_index in range(top_rock_row_index // 2):
-                mid_index = start_index + (top_rock_row_index - start_index) // 2
-                if np.array_equal(cave[start_index], cave[top_rock_row_index]) and np.array_equal(cave[start_index], cave[mid_index]):
-                    cycle_len = mid_index - start_index
-                    cycle_height_start = top_rock_row_index
-                    cycle_rock_index_start = rocks_counter
-        elif cycle_rock_index_end == 0 and top_rock_row_index == cycle_height_start + cycle_len:
-            cycle_rocks_delta = rocks_counter - cycle_rock_index_start
-            remaining_rocks_count_cycles = (orig_rocks_limit - rocks_counter) // cycle_rocks_delta
-            rocks_left = (orig_rocks_limit - rocks_counter) % cycle_rocks_delta
-            assert orig_rocks_limit == rocks_counter + remaining_rocks_count_cycles * cycle_rocks_delta + rocks_left
-            force_simulate = True
-            rocks_counter += remaining_rocks_count_cycles * cycle_rocks_delta
+        if state_key not in states.keys():
+            states[state_key] = state_val
+            continue
+
+        cycle_rocks_delta = state_val[0] - states[state_key][0]
+        cycle_height_delta = state_val[1] - states[state_key][1]
+        remaining_rock_cycles_count = (orig_rocks_limit - rocks_counter) // cycle_rocks_delta
+        rocks_left = (orig_rocks_limit - rocks_counter) % cycle_rocks_delta
+        assert orig_rocks_limit == rocks_counter + remaining_rock_cycles_count * cycle_rocks_delta + rocks_left
+        force_simulate = True
+        rocks_counter += remaining_rock_cycles_count * cycle_rocks_delta
+
     res = get_first_row_with_rock(cave)
-    res += remaining_rocks_count_cycles * cycle_len
+    res += remaining_rock_cycles_count * cycle_height_delta
     return res
 
 
@@ -97,6 +102,22 @@ def get_first_row_with_rock(cave: np.ndarray) -> int:
     return last_row_with_rock_ind
 
 
+def get_top_shape(cave: np.ndarray) -> RocksRow:
+    res: list[int] = []
+    for i in range(1, 8):
+        last_row_with_rock_ind = cave.shape[0] - 1
+        while True:
+            loc = cave[last_row_with_rock_ind][i]
+            if loc == Types.STABLE or last_row_with_rock_ind == 0:
+                res.append(last_row_with_rock_ind)
+                break
+            last_row_with_rock_ind -= 1
+    min_index = min(res)
+    res = [i - min_index for i in res]
+    assert len(res) == 7
+    return typing.cast(RocksRow, tuple(res))
+
+
 def apply_jet(jet_move: str, shape_pos: tuple[int, int], shape: np.ndarray, cave: np.ndarray) -> tuple[int, int]:
     new_x_pos = shape_pos[1] - 1 if jet_move == "<" else shape_pos[1] + 1
     # hitting wall - don't move
@@ -111,9 +132,9 @@ def apply_jet(jet_move: str, shape_pos: tuple[int, int], shape: np.ndarray, cave
 
 
 def main() -> None:
-    with open("i17.txt", "r") as i:
+    with open("i17.txt") as i:
         lines = i.readlines()
-    lines = [l.rstrip('\n') for l in lines]
+    lines = [line.rstrip('\n') for line in lines]
     res = run(lines[0], orig_rocks_limit=1000000000000)
     print(res)
 
