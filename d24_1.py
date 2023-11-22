102825bb8fc8720f34f179f62ac2c6f892f2fb41
Łukasz Piątkowski <piontec@gmail.com>
2023-11-22 13:52:11 +0100

    d24-1 test OK, wrong answer

--- d24_1.py
+++ d24_1.py
@@ -1,15 +1,16 @@
+from collections import namedtuple
 import re
 from enum import IntEnum
 from typing import Iterator
 import math
-
-import nographs as nog
 
 # X, Y
 Pos = tuple[int, int]
 # blizzard_pos, blizzard_type
 Blizzard = tuple[Pos, str]
 Blizzards = list[Blizzard]
+
+State = namedtuple('State', ['pos', 't'])
 
 moves = (0, 0), (-1, 0), (0, -1), (1, 0), (0, 1)
 
@@ -55,33 +56,59 @@ def run(lines: list[str]) -> int:
                     new_y = size[1] - 2
             new_bliz = (new_x, new_y), bliz_type
             new_blizzards.append(new_bliz)
+
+        timed_blizzards[minute] = new_blizzards
         return new_blizzards
             
 
 
     def heuristic(vertex: Pos) -> float:
-         return math.dist(vertex, exit_point)
+        return math.dist(vertex, exit_point)
 
     # gets vertex, returns iterable of vertex, distance
-    def next_edges(vertex: Pos, t) -> Iterator[tuple[Pos, int]]:
-         blizzards = get_blizzards_in_time(t.path_length + 1)
-         for move in moves:
-             new_pos = vertex[0] + move[0], vertex[1] + move[1]
-             if new_pos == exit_point:
-                 yield (new_pos), 1
-             # this assumes we never return to start_point
-             # check if we don't go into a wall
-             if new_pos[0] < 1 or new_pos[1] < 1 or new_pos[0] > size[0] - 2 or new_pos[1] > size[1] - 2:
-                 continue
-             # check if we don't go into a blizzard
-             if any(new_pos == b[0] for b in blizzards):
-                 continue
-             yield new_pos, 1
+    def next_edges(vertex: Pos, t: int) -> Iterator[State]:
+        new_t = t + 1
+        blizzards = get_blizzards_in_time(new_t)
+        for move in moves:
+            new_pos = vertex[0] + move[0], vertex[1] + move[1]
+            if new_pos == exit_point:
+                yield State(new_pos, new_t)
+            # this assumes we never return to start_point
+            # check if we don't go into a wall
+            if new_pos[0] < 1 or new_pos[1] < 1 or new_pos[0] > size[0] - 2 or new_pos[1] > size[1] - 2:
+                continue
+            # check if we don't go into a blizzard
+            collision = False
+            for b in blizzards:
+                if b[0] == new_pos:
+                   collision = True
+                   break
+            if collision:
+                continue
+           
+            yield State(new_pos, new_t)
 
-    traversal = nog.TraversalAStar(next_edges, is_tree=True)
-    traversal.start_from(heuristic, start_point)
-    traversal.go_to(exit_point)
-    return traversal.path_length
+    start_state = State(start_point, 0)
+    states: set[State] = {start_state}
+    best_time = math.inf
+    checked_states: set[State] = {start_state}
+    while len(states) > 0:
+        # choose best state to evaluate
+        best_state = min(states, key=lambda s: heuristic(s.pos))
+        states.remove(best_state)
+        checked_states.add(best_state)
+        # expand it
+        next_states = set(next_edges(best_state.pos, best_state.t))
+        found = set(filter(lambda s: s.pos == exit_point, next_states))
+        if found:
+            best_time = min(best_time, min(found, key=lambda s: s.t).t)
+            next_states.difference_update(found)
+        next_states.difference_update(checked_states) 
+        states.update(next_states)
+        # prune set
+        worse_states = list(filter(lambda s: s.t > best_time, states))
+        states.difference_update(worse_states)
+    return best_time
 
 
 def main() -> None:
